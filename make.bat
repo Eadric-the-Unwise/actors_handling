37b5fa52a5cd8e6108d0f51a8f6789937a1f38a3
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2021-07-09 08:34:25 +0900

    fixed make.bat

--- make.bat
+++ make.bat
@@ -1,8 +1,8 @@
 call .\clean.bat
 
 @echo off
-@REM ENTER YOUR GBDK DIRECTOR IN @set GBDK = ...
-@set GBDK=..\..\..\gbdk\ 
+@REM ENTER YOUR GBDK DIRECTORY IN @set GBDK = ...
+@set GBDK=..\..\..\gbdk
 %GBDK%\bin\lcc.exe -autobank -Wl-j -Wm-yS -Wb-ext=.rel -Wb-v -Wl-yt0x1B -Wl-yoA -Wl-ya4 -o actors_handling.gb^
  src\*.c res\tiles\*.c
  
